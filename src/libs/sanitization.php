70c2e2c4b38784d1ec68f2683ec947fd518f01f8
Michael Yapi .A <michaelakichi@gmail.com>
2023-02-20 14:56:47 +0000

    Update sanitization.php

--- src/libs/sanitization.php
+++ src/libs/sanitization.php
@@ -1,13 +1,5 @@
-/*
-    Ici c'est simple le codedéfinit deux fonctions :
-
-array_trim qui reçoit un tableau d'éléments en entrée et qui retourne un tableau avec tous les éléments de chaîne de caractères trimés (les espaces en début et fin de chaîne sont enlevés). Si un élément est un tableau, la fonction est réappliquée à ce tableau pour le trimer aussi. Les autres types d'éléments sont simplement retournés sans modification.
-
-sanitize qui reçoit des entrées de formulaire et un ensemble de champs spécifiés (dans un tableau). Elle utilise la fonction filter_var_array pour nettoyer les données, en utilisant les filtres définis dans le tableau constant FILTERS pour le nettoyage. Si aucun champ n'est spécifié, elle utilise un filtre par défaut pour tout nettoyer. Enfin, si l'option $trim est activée, elle appelle la fonction array_trim pour retirer les espaces en début et fin de chaîne pour toutes les chaînes de caractères dans le tableau nettoyé.
-*/
-
-
 <?php
+
 const FILTERS = [
     'string' => FILTER_SANITIZE_STRING,
     'string[]' => [
@@ -34,7 +26,6 @@ const FILTERS = [
     'url' => FILTER_SANITIZE_URL,
 ];
 
-
 function array_trim(array $items): array
 {
     return array_map(function ($item) {
@@ -46,7 +37,6 @@ function array_trim(array $items): array
             return $item;
     }, $items);
 }
-
 
 function sanitize(array $inputs, array $fields = [], int $default_filter = FILTER_SANITIZE_STRING, array $filters = FILTERS, bool $trim = true): array
 {
